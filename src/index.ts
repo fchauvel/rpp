1c8b3bc21a172d6d615424ccbc598579890de1a1
Franck Chauvel <franck.chauvel@gmail.com>
2019-12-17 20:54:03 +0100

    Show the version number

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log("That's cool!");
+const infos = require('../package.json');
+
+let version = infos.version;
+if (infos.commit != null) {
+    version = version + '+git.' + infos.commit.substring(0, 7)
+}
+
+console.log('RPP v' + version);
+console.log('Thanks!');
